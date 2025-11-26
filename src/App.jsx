bd90d1cd0420b0b3d542434de97d051b351d5762
Tokinha11 <adrianabadlopez@gmail.com>
2025-11-26 04:22:23 -0500

    Supercoin Implement

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import ProductList from './Components/ProductList';
 import ShoppingCart from './Components/ShoppingCart';
 import './App.css'
+import SuperCoin from './Components/SuperCoin'
+
 const App = () => {
   return (
  
@@ -10,6 +12,7 @@ const App = () => {
         <h1 className='app-heading'>E-Commerce Application</h1>
         <ProductList />
         <ShoppingCart />
+        <SuperCoin />
       </div>
 
   );
